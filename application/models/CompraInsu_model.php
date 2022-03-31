342cc6f4eff9c40ff0b057a9854e4b8854d52d3e
LemonKirby <101441506+LemonKirby@users.noreply.github.com>
2022-03-30 23:19:16 -0600

    Compra Insumo y Merma Insumo

---
+++ application/models/CompraInsu_model.php
@@ -0,0 +1,22 @@
+<?php
+defined('BASEPATH') OR exit('No se permite el acceso directo.');
+
+class CompraInsu_model extends CI_Model{
+
+	public function getAll(){
+		$this->db->select('idInsumo, unidadMedida, insumo.nombre as nombreinsumo, insumo.idProveedor, proveedor.nombre as nombreproveedor from insumo left join proveedor on proveedor.idProveedor = insumo.idProveedor', FALSE);
+
+		$rs = $this->db->get();
+
+		return $rs->num_rows() > 0 ? $rs->result() : NULL;
+	}
+
+	public function insertc($data){
+		return $this->db->insert('comprasinsumo', $data) ? true : NULL;
+	}
+
+	public function insertpv($data){
+		return $this->db->insert('puntoventainsumo', $data) ? true : NULL;
+	}     
+}
+

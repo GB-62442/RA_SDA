8845c3c2440c2fd6d83d9c9d0f85693f6feb1528
LemonKirby <101441506+LemonKirby@users.noreply.github.com>
2022-03-30 23:24:51 -0600

    Compra Insumo y Merma Insumo

---
+++ application/views/public/private/tabla_mermainsumo.php
@@ -0,0 +1,33 @@
+		<!-- table -->
+				<table class="table table-hover table-sm table-bordered">
+				  <thead class="table-dark">
+				    <tr>
+				      <th scope="col">#</th>
+				      <th scope="col">NOMBRE</th>
+				      <th scope="col">UNIDAD DE MEDIDA</th>
+				      <th scope="col">PROVEEDOR</th>
+				      <th scope="col">CANTIDAD</th>
+				      <th scope="col">FECHA</th>
+				    </tr>
+				  </thead>
+				  <tbody id="tabla-datos">
+				  	<?php 
+				  	if(isset($res) && !is_null($res)){
+					  	foreach ($res as $r) { ?>
+					    <tr>
+					      <th scope="row"><?php echo($r->idInsumo) ?></th>
+					      <td><?php echo($r->nombreinsumo) ?></td>
+					      <td><?php echo($r->unidadMedida) ?></td>
+					      <td><?php echo($r->nombreproveedor) ?></td> 
+					      <td><?php echo($r->cantidad) ?></td> 
+					      <td><?php echo($r->fecha) ?></td> 
+					    </tr> 
+					  	<?php
+					  	}
+				  	}
+
+				  	?>
+
+				  </tbody>
+				</table>
+ 		<!-- ./table -->

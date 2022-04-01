3965210e12dbb82515445fd52a6152844afe4e58
LemonKirby <101441506+LemonKirby@users.noreply.github.com>
2022-04-01 17:59:49 -0600

    Histórico Compra y Merma (Histórico y Agregar merma)

---
+++ static/js/app/private/modules/f_hcomprainsumo.js
@@ -0,0 +1,41 @@
+jQuery(document).ready(function ($) {
+	console.log("en -  f_hmermainsumo.js");
+
+	$(document).ready(function () {
+		console.log("ready!");
+
+		if ($("#id_registro").val() !== "") {
+			//Cargar datos
+			$.ajax({
+				url: base_url() + "insumo/get",
+				type: "post",
+				data: { id: $("#id_registro").val() },
+				cache: false,
+				dataType: "json",
+				success: function (json) {
+					if (json.resultado == "true") {
+						console.log(json);
+						$("#nombre").val(json.respuesta.nombre).change();
+					} else {
+						alert(json.respuesta);
+					}
+				},
+				error: function (ts) {
+					console.log(ts.responseText);
+					alert("Ocurrió un error, por favor vuelva a intentarlo, k");
+					clear_form();
+				},
+			});
+
+			$("#tabla-compra").load(
+				base_url() + "comprainsumo/tabla?id=" + $("#id_registro").val()
+			);
+			/*			console.log(base_url() + 'mermaproducto/tabla?id=' + $('#id_registro').val());*/
+		}
+	});
+});
+
+function clear_form() {
+	$("#form-usuarios").trigger("reset");
+	$("#id_usuario").val("").change();
+}

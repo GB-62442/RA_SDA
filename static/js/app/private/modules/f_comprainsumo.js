0a86a03bcd6bd26092d0e78d8f1f8aab1ed2631f
LemonKirby <101441506+LemonKirby@users.noreply.github.com>
2022-04-01 16:36:51 -0600

    Compras Insumo

---
+++ static/js/app/private/modules/f_comprainsumo.js
@@ -0,0 +1,96 @@
+jQuery(document).ready(function ($) {
+	console.log("en -  f_comprainsumo.js");
+
+	// cargar los proveedores
+	$(document).ready(function () {
+		console.log("ready!");
+
+		$.ajax({
+			url: base_url() + "ptventa/getall",
+			type: "post",
+			cache: false,
+			dataType: "json",
+			success: function (json) {
+				if (json.resultado == "true") {
+					$.each(json.respuesta, function (k, v) {
+						$("#select_pv").append(`<option value="${v.idPuntoVenta}">
+                            	${v.nombre}
+                            </option>`);
+					});
+				} else {
+					alert(
+						"Ocurrió un error, por favor vuelva a intentarlo, primero ingrese puntos de venta"
+					);
+				}
+			},
+			error: function (ts) {
+				console.log(ts.responseText);
+				alert("Ocurrió un error, por favor vuelva a intentarlo");
+			},
+		});
+
+		if ($("#id_registro").val() !== "") {
+			//Cargar datos
+			$.ajax({
+				url: base_url() + "insumo/get",
+				type: "post",
+				data: { id: $("#id_registro").val() },
+				cache: false,
+				dataType: "json",
+				success: function (json) {
+					if (json.resultado == "true") {
+						console.log(json);
+						$("#nombre").val(json.respuesta.nombre).change();
+					} else {
+						alert("Ocurrió un error, por favor vuelva a intentarlo,45");
+					}
+				},
+				error: function (ts) {
+					console.log(ts.responseText);
+					alert("Ocurrió un error, por favor vuelva a intentarlo, k");
+					clear_form();
+				},
+			});
+		}
+
+		/**/
+	});
+
+	/**/
+
+	$(document).on("click", ".btn-add", function (event) {
+		event.preventDefault();
+
+		$.ajax({
+			url: base_url() + "comprainsumo/insert",
+			type: "post",
+			data: {
+				idInsumo: $("#id_registro").val(),
+				cantidad: $("#cantidad").val(),
+				idPuntoVenta: $("#select_pv").val(),
+				precio: $("#precio").val(),
+			},
+			cache: false,
+			dataType: "json",
+			success: function (json) {
+				if (json.resultado == "true") {
+					alert(json.mensaje);
+					window.location = base_url() + "controlador/insumos";
+				} else {
+					alert(json.mensaje);
+				}
+			},
+			error: function (ts) {
+				console.log(ts.responseText);
+				alert("Ocurrió un error, por favor vuelva a intentarlo");
+			},
+		});
+	});
+});
+
+function clear_form() {
+	$("#form-usuarios").trigger("reset");
+	$("#id_usuario").val("").change();
+}
+
+function unblock_btn_submit() {}

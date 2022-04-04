21b26bb91361bd646433c053da19736616f5c9ae
LemonKirby <101441506+LemonKirby@users.noreply.github.com>
2022-04-03 21:26:56 -0500

    Excel y búsqueda Insumo (Merma y Compra)

--- application/models/Merma_model.php
+++ application/models/Merma_model.php
@@ -15,10 +15,17 @@ class Merma_model extends CI_Model{
 		return $this->db->insert('mermainsumo', $data) ? true : NULL;
 	}
 
- 	public function getById($id)
+ 	public function getById($id, $inicio, $final)
     {
         $cmd = "SELECT insumo.idInsumo, insumo.nombre as nombreinsumo, insumo.unidadMedida, proveedor.idProveedor, proveedor.nombre as nombreproveedor, fecha, cantidad FROM mermainsumo inner join insumo on mermainsumo.idInsumo=insumo.idInsumo inner join proveedor on proveedor.idProveedor= insumo.idProveedor where insumo.idInsumo  = ".$id;
 
+		$busqueda = "";
+        if($inicio!= null && $inicio != "" && $final != null && $final != ""){
+        	$busqueda = " AND date(fecha) BETWEEN CAST('".$inicio."' AS DATE) AND CAST('".$final."' AS DATE)";
+
+        }
+		$cmd  =$cmd.$busqueda." order by fecha desc";
+		
         $query = $this->db->query($cmd);
 		return $query->num_rows() > 0 ? $query->result() : NULL;
     }

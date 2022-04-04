5ce0b27d93fcbc35368bb4ce03359e297a52f43c
LemonKirby <101441506+LemonKirby@users.noreply.github.com>
2022-04-03 21:25:07 -0500

    Update CompraProducto.php

--- application/controllers/CompraProducto.php
+++ application/controllers/CompraProducto.php
@@ -1,6 +1,12 @@
 <?php
 defined('BASEPATH') OR exit('No se permite el acceso directo.');
 
+use PhpOffice\PhpSpreadsheet\Spreadsheet;
+use PhpOffice\PhpSpreadsheet\Writer\Xlsx;
+
+use PhpOffice\PhpSpreadsheet\Style\Alignment;
+use PhpOffice\PhpSpreadsheet\Style\Border;
+
 class CompraProducto extends CI_Controller{
 	public function __construct(){
 		parent::__construct();
@@ -110,74 +116,95 @@ class CompraProducto extends CI_Controller{
 
 
 
-  public function exportCSV(){ 
-   // file name 
-
-	$this->form_validation->set_data($_GET)->set_rules('id', 'id del producto', 'required|integer|greater_than_equal_to[1]|max_length[11]|trim');
-
-
-	if($this->form_validation->run()/* &&  $this->input->is_ajax_request()*/){
-		$id 	= $this->input->get("id");
-		$inicio = $this->input->get("inicio");
-		$fin 	= $this->input->get("fin");
-
-		$data = array(
-			"id" 	=> $id,
-			"inicio"=> $inicio,
-			"fin"	=> $fin,
-		);
-
-   		$filename = 'Comprasproductos'.date('Ymd').'('.$inicio.'-'.$fin.').csv'; 
-   		header("Content-Description: File Transfer"); 
-   		header("Content-Disposition: attachment; filename=$filename"); 
-   		header("Content-Type: application/csv; ");
-   
-   // get data 
-   		$resultData = $this->compraprod_model->getById($id, $inicio, $fin);
-
-   		// file creation 
-   		$file = fopen('php://output', 'w');
- 
-   		$col_names = array(
-   			"idProducto",
-   			"nombreproducto",
-   			"unidadMedida",
-   			"idProveedor",
-   			"nombreproveedor",
-   			"precioVenta",
-   			"costo",
-   			"fecha",
-   			"cantidad"
-   		); 
-
-     	fputcsv($file, array_values($col_names), ';', ' ');
-   		foreach ($resultData as $row){
-   		 $temp = array(
-   		 	$row->idProducto,
-   		 	$row->nombreproducto, 
-   		 	$row->unidadMedida, 
-   		 	$row->idProveedor,
-   		 	$row->nombreproveedor,
-   		 	$row->precioVenta,
-   		 	$row->costo,
-   		 	$row->fecha,
-   		 	$row->cantidad,
-   		 );
-
-     		fputcsv($file, array_values($temp), ';', ' ');
-
-   		}
-
-   		fclose($file); 
-   		exit; 
-
-/*
-object(stdClass)#23 (9) { ["idProducto"]=> string(1) "3" ["nombreproducto"]=> string(12) "Cookie Crisp" ["unidadMedida"]=> string(2) "gr" ["precioVenta"]=> string(4) "49.9" ["idProveedor"]=> string(1) "1" ["nombreproveedor"]=> string(35) "Grupo Bimbo, S.A.B. de C.V. editado" ["fecha"]=> string(19) "2022-03-31 18:58:33" ["cantidad"]=> string(2) "10" ["costo"]=> string(2) "12" }*/
-
-	} 
- 
-
-  }
+	public function exportCSV(){ 
+		// file name 
+	 
+		 $this->form_validation->set_data($_GET)->set_rules('id', 'id del producto', 'required|integer|greater_than_equal_to[1]|max_length[11]|trim');
+	 
+	 
+		 if($this->form_validation->run()/* &&  $this->input->is_ajax_request()*/){
+			 $id 	= $this->input->get("id");
+			 $inicio = $this->input->get("inicio");
+			 $fin 	= $this->input->get("fin");
+	 
+			 $data = array(
+				 "id" 	=> $id,
+				 "inicio"=> $inicio,
+				 "fin"	=> $fin,
+			 );
+		
+		// get data 
+				$resultData = $this->compraprod_model->getById($id, $inicio, $fin);
+	 
+				$spreadsheet = new Spreadsheet();
+			 $sheet = $spreadsheet->getActiveSheet();
+			 $sheet->setCellValue('A1', 'idProducto');
+			 $sheet->setCellValue('B1', 'nombreproducto');
+			 $sheet->setCellValue('C1', 'unidadMedida');
+			 $sheet->setCellValue('D1', 'idProveedor');
+			 $sheet->setCellValue('E1', 'nombreproveedor');
+			 $sheet->setCellValue('F1', 'precioVenta');
+			 $sheet->setCellValue('G1', 'costo');
+			 $sheet->setCellValue('H1', 'fecha');
+			 $sheet->setCellValue('I1', 'cantidad');
+	 
+			 $start = 2;
+			 foreach ($resultData as $row){
+			  $sheet->setCellValue('A'.$start, $row->idProducto);
+			  $sheet->setCellValue('B'.$start, $row->nombreproducto);
+			  $sheet->setCellValue('C'.$start, $row->unidadMedida);
+			  $sheet->setCellValue('D'.$start, $row->idProveedor);
+			  $sheet->setCellValue('E'.$start, $row->nombreproveedor);
+			  $sheet->setCellValue('F'.$start, $row->precioVenta);
+			  $sheet->setCellValue('G'.$start, $row->costo);
+			  $sheet->setCellValue('H'.$start, $row->fecha);
+			  $sheet->setCellValue('I'.$start, $row->cantidad);
+			  $start = $start+1;
+			 }
+	 
+			 $styleThinBlackBorderOutline = [
+				 'borders' => [
+					 'allBorders' => [
+						 'borderStyle' => Border::BORDER_THIN,
+						 'color' => ['argb' => 'FF000000'],
+					 ],
+				 ],
+			 ];
+				 //Font BOLD
+				 $sheet->getStyle('A1:I1')->getFont()->setBold(true);		
+				 $sheet->getStyle('A1:D10')->applyFromArray($styleThinBlackBorderOutline);
+				 //Alignment
+				 //fONT SIZE
+				 $sheet->getStyle('A1:D10')->getFont()->setSize(12);
+				 $sheet->getStyle('A1:G2')->getAlignment()->setVertical(Alignment::VERTICAL_CENTER);
+	 
+				 $sheet->getStyle('A2:D100')->getAlignment()->setHorizontal(Alignment::HORIZONTAL_CENTER);
+				 //Custom width for Individual Columns
+				 $sheet->getColumnDimension('A')->setWidth(20);
+				 $sheet->getColumnDimension('B')->setWidth(20);
+				 $sheet->getColumnDimension('C')->setWidth(20);
+				 $sheet->getColumnDimension('D')->setWidth(20);
+				 $sheet->getColumnDimension('E')->setWidth(20);
+				 $sheet->getColumnDimension('F')->setWidth(20);
+				 $sheet->getColumnDimension('G')->setWidth(20);
+				 $sheet->getColumnDimension('H')->setWidth(20);
+				 $sheet->getColumnDimension('I')->setWidth(20);
+	 
+				 $writer = new Xlsx($spreadsheet);
+	 
+				 $filename = 'Comprasproductos'.date('Ymd').'('.$inicio.'-'.$fin.').xlsx'; 
+				header("Content-Description: File Transfer"); 
+				header("Content-Disposition: attachment; filename=$filename"); 
+				header("Content-Type: application/vnd.ms-excel ");
+	 
+				$writer->save('php://output');
+	 /*
+	 object(stdClass)#23 (9) { ["idProducto"]=> string(1) "3" ["nombreproducto"]=> string(12) "Cookie Crisp" ["unidadMedida"]=> string(2) "gr" ["precioVenta"]=> string(4) "49.9" ["idProveedor"]=> string(1) "1" ["nombreproveedor"]=> string(35) "Grupo Bimbo, S.A.B. de C.V. editado" ["fecha"]=> string(19) "2022-03-31 18:58:33" ["cantidad"]=> string(2) "10" ["costo"]=> string(2) "12" }*/
+	 
+		 } 
+	  
+	 
+	   }
 
 
 }

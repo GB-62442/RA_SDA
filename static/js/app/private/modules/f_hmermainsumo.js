d90edc287c617072a9753a0ae2f50eed8c9f8bbe
LemonKirby <101441506+LemonKirby@users.noreply.github.com>
2022-04-03 21:21:52 -0500

    Excel y búsqueda Insumo (Merma y Compra)

--- static/js/app/private/modules/f_hmermainsumo.js
+++ static/js/app/private/modules/f_hmermainsumo.js
@@ -1,37 +1,61 @@
 jQuery(document).ready(function ($) {
 	console.log("en -  f_hmermainsumo.js");
+	console.log("ready!");
 
-	$(document).ready(function () {
-		console.log("ready!");
+	if ($("#id_registro").val() !== "") {
+		//Cargar datos
+		$.ajax({
+			url: base_url() + "insumo/get",
+			type: "post",
+			data: { id: $("#id_registro").val() },
+			cache: false,
+			dataType: "json",
+			success: function (json) {
+				if (json.resultado == "true") {
+					console.log(json);
+					$("#nombre").val(json.respuesta.nombre).change();
+				} else {
+					alert(json.respuesta);
+				}
+			},
+			error: function (ts) {
+				console.log(ts.responseText);
+				alert("Ocurrió un error, por favor vuelva a intentarlo, k");
+				clear_form();
+			},
+		});
 
-		if ($("#id_registro").val() !== "") {
-			//Cargar datos
-			$.ajax({
-				url: base_url() + "insumo/get",
-				type: "post",
-				data: { id: $("#id_registro").val() },
-				cache: false,
-				dataType: "json",
-				success: function (json) {
-					if (json.resultado == "true") {
-						console.log(json);
-						$("#nombre").val(json.respuesta.nombre).change();
-					} else {
-						alert(json.respuesta);
-					}
-				},
-				error: function (ts) {
-					console.log(ts.responseText);
-					alert("Ocurrió un error, por favor vuelva a intentarlo, k");
-					clear_form();
-				},
-			});
+		$("#tabla-datosinsu").load(
+			base_url() + "merma/tabla?id=" + $("#id_registro").val()
+		);
+		/*			console.log(base_url() + 'mermaproducto/tabla?id=' + $('#id_registro').val());*/
+	}
 
-			$("#tabla-datosinsu").load(
-				base_url() + "merma/tabla?id=" + $("#id_registro").val()
-			);
-			/*			console.log(base_url() + 'mermaproducto/tabla?id=' + $('#id_registro').val());*/
-		}
+	$(document).on("click", ".btn-search", function (event) {
+		event.preventDefault();
+
+		$("#tabla-datosinsu").load(
+			base_url() +
+				"merma/tabla?id=" +
+				$("#id_registro").val() +
+				"&inicio=" +
+				$("#inicio").val() +
+				"&fin=" +
+				$("#final").val()
+		);
+	});
+
+	$(document).on("click", ".btn-export", function (event) {
+		event.preventDefault();
+		window.open(
+			base_url() +
+				"merma/exportCSV?id=" +
+				$("#id_registro").val() +
+				"&inicio=" +
+				$("#inicio").val() +
+				"&fin=" +
+				$("#final").val()
+		);
 	});
 });
 
